3ace48e73df4f2d33adc2efb579997e0e8e8e6d6
Amenly <60410674+Amenly@users.noreply.github.com>
2021-02-08 19:19:36 -0600

    Update logger.py

--- logs/logger.py
+++ logs/logger.py
@@ -5,13 +5,27 @@ import platform
 
 from blessed import Terminal
 
+_status_error = '''
+There was an error connecting to LoyalFans's servers. This could either be because their servers are down or because your 'authorization' cookie has expired.'''
+
+_key_error = f'''
+There was an error retrieving data from the LoyalFans API. This could possibly be due to a change in their API.
+Please file an issue on Github at:
+https://github.com/Amenly/LoyalFans/issues/new
+
+An error log was created/updated at '{os.path.join(sys.path[0], 'error.log')}'. Please copy and paste the message in the log file to your issue. The traceback may contain your name/username; if you need to, you can remove those parts.'''
+
+_error_format = f'''
+DATE: %(asctime)s
+LEVEL: %(levelname)s
+SYSTEM: {platform.platform()}
+PYTHON VERSION: {platform.python_version()}
+ERROR MESSAGE: %(message)s'''
+
 
 class Logger:
-    status_error = "There was an error connecting to LoyalFans's servers. This could either be because their servers are down or because your 'authorization' cookie has expired."
-    key_error = f"There was an error retrieving data from the LoyalFans API. This could be due to them changing their API. Please file an issue on Github at:\nhttps://github.com/Amenly/LoyalFans/issues/new\nAn error log was created and updated at {os.path.join(sys.path[0], 'error.log')}. Please copy and paste the content pertaining to this issue. The traceback may contain your name/username; if you need to, you can remove those parts."
-
-    wait1 = "Please be patient, this user may have a lot of content..."
-    wait2 = "Still scraping..."
+    STATUS_ERROR = _status_error
+    KEY_ERROR = _key_error
 
     def __init__(self, debug):
         self.term = Terminal()
@@ -23,19 +37,20 @@ class Logger:
             self.logger.setLevel(logging.INFO)
             INFO_FORMAT = "%(message)s"
         DATE_FORMAT = "%b-%d-%Y %I:%M:%S %p"
-        ERROR_FORMAT = f"%(asctime)s:{self.term.red('%(levelname)s')}:{platform.platform()}:{platform.python_version()}\nError: {self.term.red('%(message)s')}"
-        info_formatter = logging.Formatter(fmt=INFO_FORMAT)
-        error_formatter = logging.Formatter(
-            fmt=ERROR_FORMAT, datefmt=DATE_FORMAT)
-        stream_handler = logging.StreamHandler()
-        file_handler = logging.FileHandler(
-            os.path.join(sys.path[0], "error.log"))
-        stream_handler.setLevel(logging.DEBUG)
-        file_handler.setLevel(logging.ERROR)
-        stream_handler.setFormatter(info_formatter)
-        file_handler.setFormatter(error_formatter)
-        self.logger.addHandler(stream_handler)
-        self.logger.addHandler(file_handler)
+        ERROR_FORMAT = _error_format
+        if not self.logger.handlers:
+            info_formatter = logging.Formatter(fmt=INFO_FORMAT)
+            error_formatter = logging.Formatter(
+                fmt=ERROR_FORMAT, datefmt=DATE_FORMAT)
+            stream_handler = logging.StreamHandler()
+            file_handler = logging.FileHandler(
+                os.path.join(sys.path[0], "error.log"))
+            stream_handler.setLevel(logging.DEBUG)
+            file_handler.setLevel(logging.ERROR)
+            stream_handler.setFormatter(info_formatter)
+            file_handler.setFormatter(error_formatter)
+            self.logger.addHandler(stream_handler)
+            self.logger.addHandler(file_handler)
 
     def error(self, message):
         self.logger.error(message, exc_info=1)
